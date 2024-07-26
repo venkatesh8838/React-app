2a69293278942dc328c738f80a07a137f2d98270
Suneel Kumar <138564160+suneelprojects@users.noreply.github.com>
2024-07-26 20:58:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to FLM Class
+          Edit <code>src/App.js</code> Welcome to FLM Class - Amplfiy-React Deployment
         </p>
         <a
           className="App-link"
