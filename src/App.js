6da7466eb287c2141c8f46676d2d22d75a56511e
Suneel Kumar <138564160+suneelprojects@users.noreply.github.com>
2025-07-18 20:09:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Your First React Application
+          Edit <code>src/App.js</code> Welcome to FLM!!
         </p>
         <a
           className="App-link"
